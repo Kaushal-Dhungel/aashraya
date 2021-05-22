f04122875117a4a82d7928542b2ba314b8ed0ebe
Kaushal-Dhungel <hazardfanboy91@gmail.com>
2021-05-22 13:14:56 +0545

    improved test suite for searchingapp

--- searchingapp/tests/test_views.py
+++ searchingapp/tests/test_views.py
@@ -5,8 +5,8 @@ from datetime import date,timedelta
 from searchingapp.models import *
 import json
 
-from io import StringIO, BytesIO
-from django.core.files import File
+# from io import StringIO, BytesIO
+# from django.core.files import File
 # from django.utils.six import BytesIO
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.conf import settings
@@ -28,7 +28,7 @@ from django.conf import settings
 #    image_file = ContentFile(data.read())
 #    return storage.save(filename, image_file)
 
-class TestItemView(APITestCase):
+class CommonTestClass(APITestCase):
 
     def setUp(self):
         self.user = User.objects.create(username = "testUserrrr",password = "testinguser12345")
@@ -36,7 +36,12 @@ class TestItemView(APITestCase):
         self.item = Item.objects.create(profile = self.profile, category = "room",headline = "dummy title",location = " abc,def",location_customised = "abc123",
                                     latitude = "30",longitude = "30",price = 60.0, details = "dummy text", slug = "dummy")
         self.cartItem = CartItem.objects.create(item = self.item,profile = self.profile)
-        self.url = reverse('itemView',args = ['room'])
+        self.client.force_authenticate(user = self.user)
+
+
+class TestItemView(CommonTestClass):
+
+    url = reverse('itemView',args = ['room'])
 
     def test_get(self):
         data = {'city':'abcdef','minPrice':30 , 'maxPrice': 90}
@@ -45,16 +50,9 @@ class TestItemView(APITestCase):
 
     # todo test get with differernt filter in minPrice and maxPrice
 
-class TestViewItem(APITestCase):
+class TestViewItem(CommonTestClass):
 
-    def setUp(self):
-        self.user = User.objects.create(username = "testUserrrr",password = "testinguser12345")
-        self.profile = self.user.profile
-        self.item = Item.objects.create(profile = self.profile, category = "room",headline = "dummy title",location = " abc,def",location_customised = "abc123",
-                                    latitude = "30",longitude = "30",price = 60.0, details = "dummy text", slug = "dummy")
-        self.cartItem = CartItem.objects.create(item = self.item,profile = self.profile)
-        self.url = reverse('viewItem')
-        self.client.force_authenticate(user = self.user)
+    url = reverse('viewItem')
 
     def test_get(self):
         res = self.client.get(self.url)
@@ -81,28 +79,24 @@ class TestViewItem(APITestCase):
         self.assertEquals(res.status_code,201)
 
 
-class TestItemDetailiew(APITestCase):
+class TestItemDetailiew(CommonTestClass):
 
-    def setUp(self):
-        self.user = User.objects.create(username = "testUserrrr",password = "testinguser12345")
-        self.profile = self.user.profile
-        self.item = Item.objects.create(profile = self.profile, category = "room",headline = "dummy title",location = " abc,def",location_customised = "abc123",
-                                    latitude = "30",longitude = "30",price = 60.0, details = "dummy text", slug = "dummy")
+    img_path = settings.BASE_DIR/'media/People/elon.jpg'
+    image_file = SimpleUploadedFile(name='elon.jpg', content=open(img_path, 'rb').read(), content_type='image/jpeg')
 
-        img_path = settings.BASE_DIR/'media/People/elon.jpg'
-        self.image_file = SimpleUploadedFile(name='elon.jpg', content=open(img_path, 'rb').read(), content_type='image/jpeg')
-        self.img = Image.objects.create(item = self.item,image = self.image_file)
-        self.cartItem = CartItem.objects.create(item = self.item,profile = self.profile)
-        self.url = reverse('itemDetails',args= [self.item.slug])
-        self.client.force_authenticate(user = self.user)
+    def get_img(self):
+        return Image.objects.create(item = self.item,image = self.image_file)
+
+    def get_url(self):
+        return reverse('itemDetails',args= [self.item.slug])
 
     def test_get(self):
-        res = self.client.get(self.url)
+        res = self.client.get(self.get_url())
         self.assertEquals(res.status_code,200)
 
     def test_post_remove_img(self):
-        data = {'item_id': self.item.id,'action':'remove_img', 'img_id': self.img.id}
-        res = self.client.post(self.url,data)  # putting format = json gives error idk why
+        data = {'item_id': self.item.id,'action':'remove_img', 'img_id': self.get_img().id}
+        res = self.client.post(self.get_url(),data)  # putting format = json gives error idk why
         self.assertEquals(res.status_code,201) 
 
     #TODO
@@ -127,26 +121,19 @@ class TestItemDetailiew(APITestCase):
             'slug': self.item.slug  
         }
 
-        res =  self.client.put(self.url,data)
+        res =  self.client.put(self.get_url(),data)
         self.assertEquals(res.data['details'],'dummy details updated') 
         self.assertEquals(res.status_code,201) 
 
     def test_delete(self):
-        res =  self.client.delete(self.url)
+        res =  self.client.delete(self.get_url())
         self.assertEquals(res.data,{'deleted'}) 
         self.assertEquals(res.status_code,200) 
 
 
-class TestCartView(APITestCase):
+class TestCartView(CommonTestClass):
 
-    def setUp(self):
-        self.user = User.objects.create(username = "testUserrrr",password = "testinguser12345")
-        self.profile = self.user.profile
-        self.item = Item.objects.create(profile = self.profile, category = "room",headline = "dummy title",location = " abc,def",location_customised = "abc123",
-                                    latitude = "30",longitude = "30",price = 60.0, details = "dummy text", slug = "dummy")
-        self.cartItem = CartItem.objects.create(item = self.item,profile = self.profile)
-        self.url = reverse('cartview')
-        self.client.force_authenticate(user = self.user)
+    url = reverse('cartview')
 
     def test_get(self):
         res = self.client.get(self.url)
