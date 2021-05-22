8716f625a3b4a917464786866976fca9cfea84f1
Kaushal-Dhungel <hazardfanboy91@gmail.com>
2021-05-22 18:28:25 +0545

    made some update in idex.html

--- myproject/asgi.py
+++ myproject/asgi.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from .routing import application as app
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
